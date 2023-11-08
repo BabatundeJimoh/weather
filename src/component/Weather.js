e2673d072fedd88b06a7c129ace403991ff8d5c0
babatunde <benjee135333@gmail.com>
2023-11-08 14:00:38 +0000

    weather done

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -33,30 +33,30 @@ const Weather = () => {
     if (temperature < 10) {
       weatherImage = (
         <>
-          <ion-icon
-            name="rainy-outline"
+          <img
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAepJREFUaN7tmd2thCAQhSnBEijBEmjgJpZgCZZgCZZgCXZwLcFX3yiBDlhIhhvC5dcdFzeB5LzsTtjz4cwwZomUknyzSANoAA2gATQANICSdZ4nA/Wk0ioGUGY7pVVJKElLXGl6NIAySMGojGh9MsCRMG80Pw5AmRozzRvppzVoGNCk068mwFYI4JPArpMSgB0BwGj5doAVutnsdDNdYwwVAH5o8rTNqzoyGsKIAqAvKETjRkNGQxC5BR8EuMm8vJCO0cKPAXBs83DyXcF9YmvKBoDHfIf5/o2DESUAM7L5HU6eez4fraFwzEwvYQo9BLDfcPrM2dek02jd1vZnOfuyEMCKaH52OwqMGaF04vBdDsQaApgQT546Jy9g/1gtcHgSqUxYQgD0xrwvgR9gn5D6WBtdEC6s6e55KQbQWd3hijqECVakxoq33okzXj03pFpiNQCOT4zfSYCf35MprUpHaBMrhltzlKwOoAx1SkJJgnqPeR3DrRhmXYZY6q8CLJaxP3NOzOzEjOSDKwigT9sxtnvM01RMTYDdMUc9AG5M/wgAnQaOsdljfnBiFlJh/QPwFK4u0C5RuMKNqQngFu7wtMJNAYhUUebE1ATYYoULAFvNwk0BmFuVZty8lFRe7S+mBtAAGkADeFsvKpKWeAy6FowAAAAASUVORK5CYII="
             style={{ fontSize: "50px" }}
-          ></ion-icon>
+          />
           <p>Rainy</p>
         </>
       );
     } else if (temperature >= 10 && temperature < 20) {
       weatherImage = (
         <>
-          <ion-icon
-            name="cloudy-outline"
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
             style={{ fontSize: "50px" }}
-          ></ion-icon>
-          <p>Normal</p>
+          />
+          <p>Cloudy</p>
         </>
       );
     } else {
       weatherImage = (
         <>
-          <ion-icon
-            name="sunny-outline"
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
             style={{ fontSize: "50px" }}
-          ></ion-icon>
+          />
           <p>Sunny</p>
         </>
       );
@@ -77,103 +77,119 @@ const Weather = () => {
   };
 
   return (
-    <div>
-      <Container fluid className="hello">
-        <Row>
-          <Col sm={7}>
-            <p style={{ paddingTop: "20px" }}>
-              <b>WeatherApp</b>
-            </p>
-            {weatherData ? (
-              <div
-                style={{
-                  paddingTop: "430px",
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <h1>
-                  <b>{weatherData.main.temp}°</b>
-                </h1>
-                <h2 style={{ paddingTop: "10px", paddingRight: "20px" }}>
-                  {weatherData.name}
-                </h2>
-                <p style={{ paddingBottom: "20px" }}>{weatherImage}</p>
-              </div>
-            ) : (
-              <p>Loading weather data...</p>
-            )}
-          </Col>
-          <Col sm={5} className="list" style={{ height: "100vh" }}>
-            <div>
-              <Form inline>
-                <Row>
-                  <Col>
-                    <input
-                      type="text"
-                      className="search-input"
-                      placeholder="Search.."
-                      style={{
-                        width: "400px",
-                        background: "rgba(255, 255, 255, 0.5)",
-                        backdropFilter: "blur(0px)",
-                        border: "none",
-                        borderRadius: "5px",
-                        color: "white",
-                        height: "60px",
-                      }}
-                      value={inputValue}
-                      onChange={(e) => setInputValue(e.target.value)}
-                    />
-                  </Col>
-                  <Col style={{ position: "relative" }}>
-                    <Button
-                      type="button"
-                      style={{
-                        height: "60px",
-                        backgroundColor: "white",
-                        color: "black",
-                      }}
-                      onClick={handleSearch}
-                    >
-                      <ion-icon
-                        name="search-outline"
-                        style={{ fontSize: "30px" }}
-                      ></ion-icon>
-                    </Button>
-                  </Col>
-                </Row>
-              </Form>
-              <br />
-              <p>
-                <b>Weather Details</b>
-              </p>
-              <br />
-              {weatherData && (
-                <div>
-                  <p>Humidity: {weatherData.main.humidity}%</p>
-                  <br />
-                  <p>Wind Speed: {weatherData.wind.speed} m/s</p>
-                  <br />
-                  <p>
-                    Weather Description: {weatherData.weather[0].description}
-                  </p>
-                  <br />
-                  <p>Country: {weatherData.sys.country}</p>
-                  <br />
-                  <hr />
-                  <p>Sunrise: {sunriseTime}</p>
-                  <br />
-                  <p>Sunset: {sunsetTime}</p>
-                  <br />
-                </div>
-              )}
-              <p> {currentDateTime}</p>
+    <Container fluid className="hello">
+      <Row>
+        <Col lg={7} md={12}>
+          {" "}
+          {/* Adjust column widths for medium screens */}
+          <p style={{ paddingTop: "20px" }}>
+            <b>WeatherApp</b>
+          </p>
+          {weatherData ? (
+            <div
+              style={{
+                paddingTop: "120px" /* Reduce the top padding */,
+                display: "flex",
+                justifyContent: "center",
+                flexDirection:
+                  "column" /* Center vertically on small screens */,
+
+                alignItems: "center",
+              }}
+            >
+              <h1>
+                <b>{weatherData.main.temp}°</b>
+              </h1>
+              <h2 style={{ paddingTop: "10px", paddingRight: "20px" }}>
+                {weatherData.name}
+              </h2>
+              <p style={{ paddingBottom: "20px" }}>{weatherImage}</p>
             </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+          ) : (
+            <p>Loading weather data...</p>
+          )}
+        </Col>
+        <Col
+          lg={5}
+          md={12}
+          className="list"
+          style={{
+            backgroundColor: " background-color: rgb(102, 190, 202)",
+            minHeight: "100vh",
+          }}
+        >
+          <div>
+            <Form inline>
+              <Row>
+                <Col xs={8} md={8}>
+                  {" "}
+                  {/* Adjust column widths for small and medium screens */}
+                  <input
+                    type="text"
+                    className="search-input"
+                    placeholder="Search.."
+                    style={{
+                      width:
+                        "100%" /* Make the input full width on small screens */,
+                      background: "rgba(255, 255, 255, 0.5)",
+                      backdropFilter: "blur(0px)",
+                      border: "none",
+                      borderRadius: "5px",
+                      color: "white",
+                      height: "60px",
+                    }}
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                  />
+                </Col>
+                <Col xs={4} md={4}>
+                  {" "}
+                  {/* Adjust column widths for small and medium screens */}
+                  <Button
+                    type="button"
+                    style={{
+                      height: "60px",
+                      backgroundColor: "white",
+                      color: "black",
+                    }}
+                    onClick={handleSearch}
+                  >
+                    <ion-icon
+                      name="search-outline"
+                      style={{ fontSize: "30px" }}
+                    ></ion-icon>
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+
+            <br />
+            <p>
+              <b>Weather Details</b>
+            </p>
+            <br />
+            {weatherData && (
+              <div>
+                <p>Humidity: {weatherData.main.humidity}%</p>
+                <br />
+                <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+                <br />
+                <p>Weather Description: {weatherData.weather[0].description}</p>
+                <br />
+                <p>Country: {weatherData.sys.country}</p>
+                <br />
+                <hr />
+                <p>Sunrise: {sunriseTime}</p>
+                <br />
+                <p>Sunset: {sunsetTime}</p>
+                <br />
+              </div>
+            )}
+            <p> {currentDateTime}</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
